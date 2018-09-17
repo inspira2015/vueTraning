75cb80372357ee9e12e907fee58d712329aff6d5
Daniel Gomez <dgomez@nassaucountynh.gov>
2018-09-17 14:53:24 -0400

    Completed Excercise

--- Exercise_3.php
+++ Exercise_3.php
@@ -20,6 +20,19 @@
         el: '#exercise',
         data: {
             value: 0
+        },
+        computed: {
+            result: function() {
+                return this.value == 37 ? "done" : "not there yet"
+            },
+        },
+        watch: {
+            result: function() {
+                var vm = this;
+                setTimeout(function() {
+                    vm.value = 0;
+                }, 5000);
+            }
         }
     });
 </script>
