667c9f07f86fca37d6856731296eabf05494d7d3
Daniel Gomez <dgomez@nassaucountynh.gov>
2018-09-18 15:02:57 -0400

    Monster Project - Special Attack - DRY

--- app_monster.js
+++ app_monster.js
@@ -18,8 +18,7 @@ new Vue({
 				return;
 			}
 
-			this.playerHealth -= this.calculateDamage(5, 12);
-			this.checkWin();
+			this.monsterAttacks();
 		},
 		specialAttack: function() {
 			this.monsterHealth -= this.calculateDamage(10, 20);
@@ -27,8 +26,7 @@ new Vue({
 			if (this.checkWin()) {
 				return;
 			}
-			this.playerHealth -= this.calculateDamage(5, 12);
-			this.checkWin();
+			this.monsterAttacks();
 		},
 		heal: function() {
 
@@ -37,7 +35,8 @@ new Vue({
 
 		},
 		monsterAttacks: function() {
-
+			this.playerHealth -= this.calculateDamage(5, 12);
+			this.checkWin();
 		},
 		calculateDamage: function(min, max) {
 			return Math.max(Math.floor(Math.random() * max) + 1, min);
