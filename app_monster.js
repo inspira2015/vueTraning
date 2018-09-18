6928e3ee4ee09c58759ef72b5a6ac57703566093
Daniel Gomez <dgomez@nassaucountynh.gov>
2018-09-18 14:43:32 -0400

    Monster Project WET

--- app_monster.js
+++ app_monster.js
@@ -12,7 +12,15 @@ new Vue({
 			this.monsterHealth = 100;
 		},
 		attack: function() {
+			var max = 10;
+			var min = 3;
+			var damage = Math.max(Math.floor(Math.random() * max) + 1, min);
+			this.monsterHealth -= damage;
 
+			max = 12;
+			min = 5;
+			var damage = Math.max(Math.floor(Math.random() * max) + 1, min);
+			this.playerHealth -= damage;
 		},
 		specialAttack: function() {
 
