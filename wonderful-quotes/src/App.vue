ca475a9b6dcb9641fa267e7d857c5bb0d37e6e24
Daniel Gomez <dgomez@nassaucountynh.gov>
2018-09-20 14:16:40 -0400

    Wonderful Quotes Project   Setup

---
+++ wonderful-quotes/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="container">
+    	<app-quote-grid :quotes="quotes"> </app-quote-grid>
+    </div>
+</template>
+
+<script>
+	import QuoteGrid from './components/QuoteGrid.vue';
+
+    export default {
+        data: function() {
+        	return {
+        		maxQuotes: 10,
+        		quotes: [
+        				'default Quote'
+        		],
+        	}
+        },
+        components: {
+        	appQuoteGrid: QuoteGrid
+        }
+    }
+</script>
+
+<style>
+</style>
