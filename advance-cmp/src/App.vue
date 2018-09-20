ebcc1f775e8a3f89a63940f18994764f3165b743
Daniel Gomez <dgomez@nassaucountynh.gov>
2018-09-20 12:40:53 -0400

    Advanced Component Usage

---
+++ advance-cmp/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+               <app-quote quote="A wonderful quote!"></app-quote>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+	import quote from './components/quote.vue';
+    export default {
+     	components: {
+     		appQuote: quote,
+     	}   
+    }
+</script>
+
+<style>
+</style>
