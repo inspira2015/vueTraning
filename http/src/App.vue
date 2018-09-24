656987eeec8a86f48c0ac76468ca5908c0bb45fa
Daniel Gomez <dgomez@nassaucountynh.gov>
2018-09-24 14:23:50 -0400

    Vue http Sending Post Request Firebase

--- http/src/App.vue
+++ http/src/App.vue
@@ -29,7 +29,12 @@
     	},
     	methods: {
     		submit() {
-    			console.log(this.user);
+    			this.$http.post('https://vuejs-app-ed382.firebaseio.com/data.json', this.user)
+    				.then(response => {
+    					console.log(response);
+    				}, error => {
+    					console.log(error);
+    				});
     		}
     	}
     }
