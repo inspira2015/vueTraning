6ceff8598b63ef281566683d9cb201cdd8eb3d0b
Daniel Gomez <dgomez@nassaucountynh.gov>
2018-09-24 13:24:24 -0400

    Duplicate filter problem

--- filters-mixins/src/List.vue
+++ filters-mixins/src/List.vue
@@ -1,35 +1,20 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <div>
                 <h1>Filters & Mixins</h1>
-                <p>{{ text | toUppercase | to-lowercase }}</p>
-                <hr>
                 <input v-model="filterText">
                 <ul>
                 	<li v-for="fruit in filteredFruits">{{ fruit }}</li>
                 </ul>
-                <hr>
-                <app-list></app-list>
-            </div>
         </div>
-    </div>
+  
 </template>
 
 <script>
-	import List from './List.vue';
-
     export default {
     	data() {
     		return {
-    			text: 'Hello there!',
     			fruits: ['Apple', 'Bannana', 'Mango', 'Melon', 'Orange'],
     			filterText: ''
-    		}
-    	},
-    	filters: {
-    		toUppercase(value) {
-    			return value.toUpperCase();
     		}
     	},
     	computed: {
@@ -38,9 +23,6 @@
     				return element.match(this.filterText);
     			});
     		}
-    	},
-    	components:{
-    		appList: List,
     	}
     }
 </script>
