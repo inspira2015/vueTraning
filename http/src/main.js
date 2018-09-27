0c70c0a05bea0620b53fed731690faabfee9d724
Daniel Gomez <dgomez@nassaucountynh.gov>
2018-09-27 13:21:07 -0400

    Vue http Adding global Url for request

--- http/src/main.js
+++ http/src/main.js
@@ -5,6 +5,8 @@ import VueResource from 'vue-resource';
 
 Vue.use(VueResource);
 
+Vue.http.options.root = 'https://vuejs-app-ed382.firebaseio.com/data.json';
+
 new Vue({
   el: '#app',
   render: h => h(App)
