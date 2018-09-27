747cb97956383976c7582720e3a718044ff4e9b1
Daniel Gomez <dgomez@nassaucountynh.gov>
2018-09-27 13:16:19 -0400

    Vue http Fetching Get data from Firebase

--- http/src/App.vue
+++ http/src/App.vue
@@ -12,7 +12,14 @@
                 	<input class="form-control" type="text" v-model="user.email">
                 </div>
                 <button class="btn btn-primary" @click="submit">Submit</button>
+                <hr>
+                <button class="btn btn-primary" @click="fetchData">Get Data</button>
+                <br><br>
+                <ul class="list-group">
+                    <li class="list-group-item" v-for="(u, index) in users">{{ u.username }} - {{ u.email }}</li>
+                </ul>
             </div>
+
         </div>
     </div>
 </template>
@@ -24,7 +31,8 @@
     			user: {
     				username: '',
     				email: '',
-    			}
+    			},
+                users: []
     		};
     	},
     	methods: {
@@ -35,7 +43,25 @@
     				}, error => {
     					console.log(error);
     				});
-    		}
+    		},
+            fetchData() {
+                this.$http.get('https://vuejs-app-ed382.firebaseio.com/data.json')
+                    .then(response => {
+                        return response.json();
+                    })
+                    .then(data => {
+                        const resultArray = [];
+                        for (let key in data) {
+                            resultArray.push(data[key]);
+                        }
+                        this.users = resultArray;
+                        console.log(data);
+                    })
+                    .catch(error => {
+                        console.log('error here');
+                        console.log(error);
+                    });
+            }
     	}
     }
 </script>
--- http/src/main.js
+++ http/src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+// Added http client $http
 import VueResource from 'vue-resource';
 
 Vue.use(VueResource);
