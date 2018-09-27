63f1251f5e5aa6887904ef9b79cebe3d9cb52298
Daniel Gomez <dgomez@nassaucountynh.gov>
2018-09-27 15:00:40 -0400

    Vue Routing - From Code(Imperative Navigation)

--- routing/src/components/user/User.vue
+++ routing/src/components/user/User.vue
@@ -1,4 +1,17 @@
 <template>
-    <h1>The User Page</h1>
-
+	<div>
+    	<h1>The User Page</h1>
+		<hr>
+		<button @click="naviagateToHome">Go to Home</button>
+	</div>
 </template>
+
+<script>
+	export default {
+		methods: {
+			naviagateToHome() {
+				this.$router.push({path: '/'});
+			}
+		}
+	}
+</script>
