1ddd00f1be796858566c09ec5526b8d82aa940e4
Daniel Gomez <dgomez@nassaucountynh.gov>
2018-09-28 14:12:41 -0400

    Vue Routing - Added Multiple Router Views (Named Router View)

--- routing/src/components/user/UserDetail.vue
+++ routing/src/components/user/UserDetail.vue
@@ -2,8 +2,10 @@
 	<div>
 		<h3>Some User Details</h3>
 		<p>User Id: {{ $route.params.id }}</p>
-		<router-link tag="button" 
-					 :to="{ name: 'userEdit', params: {id: $route.params.id } }"
+		<router-link tag="button"
+					 :to="{ name: 'userEdit',
+					        params: {id: $route.params.id },
+							query: { local: 'en', q: 100 } }"
 					 class="btn btn-primary">Edit User</router-link>
 	</div>
 </template>
