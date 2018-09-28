aa18e12c56b5e75634eb00bf09ef9e1064504b3a
Daniel Mac <daniel.gm78@gmail.com>
2018-09-27 22:00:09 -0400

    Vue Routing - Saving Changes

--- routing/src/App.vue
+++ routing/src/App.vue
@@ -19,6 +19,7 @@
     		appHeader: Header,
     	}
     }
+
 </script>
 
 <style>
--- routing/src/components/user/UserDetail.vue
+++ routing/src/components/user/UserDetail.vue
@@ -2,5 +2,6 @@
 	<div>
 		<h3>Some User Details</h3>
 		<p>User Id: {{ $route.params.id }}</p>
+		<router-link tag="button" :to=" '/user/' + $route.params.id + '/edit' ">Edit User</router-link>
 	</div>
 </template>
