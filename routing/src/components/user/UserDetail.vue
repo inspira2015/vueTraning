58400ebded0270e7e7fe3e1a19e4c98b0534d38e
Daniel Gomez <dgomez@nassaucountynh.gov>
2018-09-28 08:35:56 -0400

    Vue Routing - Init VSC as IDE

--- routing/src/components/user/UserDetail.vue
+++ routing/src/components/user/UserDetail.vue
@@ -2,6 +2,8 @@
 	<div>
 		<h3>Some User Details</h3>
 		<p>User Id: {{ $route.params.id }}</p>
-		<router-link tag="button" :to=" '/user/' + $route.params.id + '/edit' ">Edit User</router-link>
+		<router-link tag="button" 
+					 :to="'/user/' + $route.params.id + '/edit'"
+					 class="btn btn-primary">Edit User</router-link>
 	</div>
 </template>
