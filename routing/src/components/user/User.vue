d2b0a79e748c6412290b01b722002bed971239ee
Daniel Mac <daniel.gm78@gmail.com>
2018-09-27 21:10:39 -0400

    Vue Routing - Added id to User route

--- routing/src/components/user/User.vue
+++ routing/src/components/user/User.vue
@@ -2,12 +2,18 @@
 	<div>
     	<h1>The User Page</h1>
 		<hr>
+		<p>Loaded ID: {{ id }}</p>
 		<button @click="naviagateToHome">Go to Home</button>
 	</div>
 </template>
 
 <script>
 	export default {
+		data() {
+				return {
+					id: this.$route.params.id
+				}
+		},
 		methods: {
 			naviagateToHome() {
 				this.$router.push({path: '/'});
