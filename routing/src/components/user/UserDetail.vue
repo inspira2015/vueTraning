1a9bb252db9b9485b2d8e4b8bcf16b71fe1db89b
Daniel Gomez <dgomez@nassaucountynh.gov>
2018-09-28 14:57:27 -0400

    Partial changes

--- routing/src/components/user/UserDetail.vue
+++ routing/src/components/user/UserDetail.vue
@@ -3,9 +3,27 @@
 		<h3>Some User Details</h3>
 		<p>User Id: {{ $route.params.id }}</p>
 		<router-link tag="button"
-					 :to="{ name: 'userEdit',
-					        params: {id: $route.params.id },
-							query: { local: 'en', q: 100 } }"
+					 :to="link"
 					 class="btn btn-primary">Edit User</router-link>
 	</div>
 </template>
+
+<script>
+export default {
+	data()	{
+		return {
+			link: {
+				name: 'userEdit',
+					params: {
+						id: this.$route.params.id
+					},
+					query: {
+						local: 'en',
+						q: 100
+					},
+					hash: '#data'
+				}
+		}
+	}
+}
+</script>
