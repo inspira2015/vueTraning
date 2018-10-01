92dd5108b82bfd59718484abad3f4d2692ef1613
Daniel Gomez <dgomez@nassaucountynh.gov>
2018-10-01 10:30:20 -0400

    Vue Routing - Protecting Routes before leaving

--- routing/src/components/user/UserEdit.vue
+++ routing/src/components/user/UserEdit.vue
@@ -3,5 +3,31 @@
         <h3>Edit the User</h3>
         <p>Locale: {{ $route.query.local }}</p>
         <p>Analitycs: {{ $route.query.q }}</p>
+        <hr>
+        <button class="btn btn-primary" @click="confirmed = true">Confirm</button>
+        <div style="height: 700px;"></div>
+        <p id="data">Some Extra Data</p>
     </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            confirmed: false
+        }
+    },
+    beforeRouteLeave(to, from, next) {
+        if (this.confirmed) {
+            next();
+        } else {
+            if (confirm('Are you sure')) {
+                next();
+            } else {
+                next(false);
+            }
+        }
+        next();
+    }
+}
+</script>
